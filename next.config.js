cd2f4f0bded8fb76b12ff13c3a2a29256c9b743b
PAP-nakyoutae <kyoutae@chicment.com>
2023-11-10 17:19:50 +0900

    수정

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
+    output: 'export',
     images: {
         loader: 'imgix',
         path: 'https://wordscount.kr/images/', // 배포 경로
-    },
-    async rewrites() {
-        return [
-            // RSS 피드를 위한 라우트 추가
-            {
-                source: '/rss.xml',
-                destination: '/rss.xml',
-            },
-        ];
     },
 }
 
